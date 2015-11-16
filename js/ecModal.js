d7ebc2cb51a95f26afb94d65d70778a6bde059eb
chensonghao <719204133@qq.com>
2015-11-16 09:59:48 +0800

    add

---
+++ js/ecModal.js
@@ -0,0 +1,104 @@
+;
+(function($) {
+    function Modal(element, options) {
+        this.opts = $.extend({}, Modal.DEFAULTS, options);
+        this.$el = element;
+        this._init();
+    }
+
+    Modal.prototype._init = function() {
+        var $el = this.$el,
+            opts = this.opts;
+        opts.target = opts.target || $el.attr('data-modal');
+        init(opts, $el);
+    }
+
+    function removeModal(callback) {
+        var md = $('.md-show');
+        md.removeClass('md-show');
+        $('.md-container').removeClass('md-blur');
+        $('html').removeClass('md-perspective');
+        if (callback instanceof Function) {
+            callback();
+        }
+    }
+
+    function init(opts, $el) {
+        if (opts.target) {
+            var overlay = $('.md-overlay');
+            var modal = $('#' + opts.target);
+            var isSetperspective = modal.hasClass('md-effect-17') || modal.hasClass('md-effect-18') || modal.hasClass('md-effect-19');
+            if ($el) {
+                $el.on('click', function(){
+                    openEvent();
+                });
+            } else {
+                openEvent();
+            }
+            modal.find('.md-close').unbind('click');
+            modal.find('.md-close').on('click', function(event) {
+                event = event || window.event;
+                removeModal(opts.closeCallback);
+                event.stopPropagation();
+            });
+
+            function openEvent() {
+                var width = modal.width();
+                var ml = width / 2;          
+                modal.css({
+                    marginLeft: -ml + 'px'
+                });
+                modal.addClass('md-show');
+                if (opts.blur) {
+                    $('.md-container').addClass('md-blur');
+                }
+                if (opts.overlayClose) {
+                    overlay.unbind('click');
+                    overlay.one('click', function() {
+                        removeModal();
+                    });
+                }
+                if (isSetperspective) {
+                    setTimeout(function() {
+                        $('html').addClass('md-perspective');
+                    }, 25);
+                }
+            }
+        }
+    }
+
+    Modal.DEFAULTS = {
+        target: '',
+        blur: false,
+        overlayClose: true,
+        closeCallback: null
+    }
+    $.fn.extend({
+        ecModal: function(opts) {
+            return this.each(function() {
+                new Modal($(this), opts);
+            });
+        }
+    });
+    $.extend({
+        ecModal: {
+            open: function(options) {
+                var opts = $.extend({}, Modal.DEFAULTS, options);
+                if (opts.beforeOpen) {
+                    opts.beforeOpen();
+                }
+                init(opts);
+                if (opts.afterOpen) {
+                    opts.afterOpen();
+                }
+            },
+            close: function(callback) {
+                removeModal(callback);
+            }
+        }
+    });
+    if ($('.md-overlay').length < 1 ) {
+        $('.md-container').after($('<div class="md-overlay"></div>'));
+    }
+    $('.md-trigger').ecModal();
+})(jQuery)
